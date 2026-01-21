2133843baf0f47431573cd15ee01151f4d05b2a9
yony-1145 <yo.hm1515@icloud.com>
2026-01-22 01:35:50 +0900

    refactor(layout): グローバルレイアウトから SideNav を削除

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import '../globals.css';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import SideNav from '@/components/SideNav';
 import { NextAuthProvider } from '@/lib/NextAuthProvider';
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body>
         <NextAuthProvider>
           <div className="flex min-h-screen">
+            <SideNav />
             <main className="flex-1 p-6 overflow-hidden">{children}</main>
           </div>
         </NextAuthProvider>
